d2ff3fa2721ad336215d4af90376673fa810a9cf
Nate Glenn <n8glenn@gmail.com>
2016-09-18 12:55:50 -0500

    added test stream

--- MajorityReport/ViewController.m
+++ MajorityReport/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import <AVFoundation/AVFoundation.h>
 
 @interface ViewController ()
 
@@ -19,7 +20,33 @@
     // Do any additional setup after loading the view, typically from a nib.
 }
 
+- (void)viewDidAppear:(BOOL)animated
+{
+    [super viewDidAppear:animated];
+    
+    NSError *setCategoryError = nil;
+    
+    /* Set the audio session category to allow for playback/recording and mixing */
+    BOOL setCategorySuccess = [[AVAudioSession sharedInstance]
+                               setCategory:AVAudioSessionCategoryPlayAndRecord
+                               withOptions: AVAudioSessionCategoryOptionMixWithOthers
+                               error:&setCategoryError];
+    
+    if (setCategorySuccess == NO) { /* handle error here */ }
+    
+    
+    NSError *activationError = nil;
+    
+    /* Activate the audio session */
+    BOOL activationResult = [[AVAudioSession sharedInstance] setActive: YES
+                                                                 error: &activationError];
+    
+    if (activationResult == NO)
+    { /* handle the error here */
+        NSLog(@"activation failed...");
+    }
 
+}
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
